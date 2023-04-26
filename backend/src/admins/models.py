19c6e3b85041c7101f82d5d65a631b1a7b4495b5
Артём Гончаров <tenagr41@gmail.com>
2023-04-26 18:25:25 +0300

    Model Admin

--- backend/src/admins/models.py
+++ backend/src/admins/models.py
@@ -1,4 +1,13 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
+from sqlalchemy import Column, String, Boolean
+
 from src.database import Base
 
 
-
+class Admin(SQLAlchemyBaseUserTableUUID, Base):
+    email: str = Column(String(length=320), unique=True, index=True, nullable=False)
+    username: str = Column(String, nullable=False)
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
